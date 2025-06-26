ca1c149775b0a3dc8f584e37bfdf0a0c848e8ede
zachglindro <zmglindro2@up.edu.ph>
2025-06-26 19:10:58 +0800

    feat: add import button in bulk-add page

--- app/bulk-add/page.tsx
+++ app/bulk-add/page.tsx
@@ -1,7 +1,19 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { FileSpreadsheet } from "lucide-react";
 
 export default function BulkAdd() {
-    return (
-        <AppSidebar />
-    )
+  return (
+    <>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        <Button variant="outline">
+          <FileSpreadsheet/>
+          Import
+        </Button>
+      </main>
+    </>
+  );
 }
