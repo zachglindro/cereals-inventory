89a2a94ec62a00c4dcdf587a3998534c9343f39a
zachglindro <zmglindro2@up.edu.ph>
2025-06-26 18:44:07 +0800

    refactor: place SidebarProvider in layout.tsx

--- app/bulk-add/page.tsx
+++ app/bulk-add/page.tsx
@@ -1,10 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function BulkAdd() {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-        </SidebarProvider>
+        <AppSidebar />
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Home() {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-        </SidebarProvider>
+        <AppSidebar />
     )
 }
