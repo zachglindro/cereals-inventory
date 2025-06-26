bb67121ee74d5f156b11f7970080e70d1d397b7f
zachglindro <zmglindro2@up.edu.ph>
2025-06-26 20:10:49 +0800

    feat(bulk-add): add import button for bulk add page

--- app/bulk-add/page.tsx
+++ app/bulk-add/page.tsx
@@ -1,16 +1,40 @@
+"use client";
+
 import { AppSidebar } from "@/components/app-sidebar";
 import { Button } from "@/components/ui/button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { FileSpreadsheet } from "lucide-react";
+import React, { useRef } from "react";
 
 export default function BulkAdd() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleImportClick = () => {
+    inputRef.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      // Do work here
+    }
+  };
+
   return (
     <>
       <AppSidebar />
+      <input
+        type="file"
+        accept=".csv, .tsv, .xlsx, .xls, .json, .ods"
+        ref={inputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
       <main>
         <SidebarTrigger />
-        <Button variant="outline">
-          <FileSpreadsheet/>
+
+        <Button variant="outline" onClick={handleImportClick}>
+          <FileSpreadsheet />
           Import
         </Button>
       </main>
