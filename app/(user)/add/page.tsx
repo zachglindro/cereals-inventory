9018299cd297166ba2aea67a30feba616c1c0ef5
zachglindro <zmglindro2@up.edu.ph>
2025-07-01 14:20:31 +0800

    refactor(add): remove SidebarInset component from Add page

--- app/(user)/add/page.tsx
+++ app/(user)/add/page.tsx
@@ -1,11 +1,9 @@
 import { InventoryForm } from "@/components/inventory-form";
-import { SidebarInset } from "@/components/ui/sidebar";
 
 export default function Add() {
   return (
     <>
       <main>
-        <SidebarInset></SidebarInset>
         <InventoryForm />
       </main>
     </>
