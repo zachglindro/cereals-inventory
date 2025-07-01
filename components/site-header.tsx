cbbbccb2feb704ada02ff8038d867a70af04006a
zachglindro <zmglindro2@up.edu.ph>
2025-07-01 21:09:55 +0800

    refactor(site-header): remove unused separator and button size props

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -2,7 +2,6 @@
 import { useSidebar } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
-import { Separator } from "@/components/ui/separator";
 
 import Image from "next/image";
 
@@ -13,14 +12,11 @@ export function SiteHeader() {
     <header className="bg-background sticky top-0 z-50 flex w-full items-center border-b">
       <div className="flex h-(--header-height) w-full items-center gap-2 px-4">
         <Button
-          className="h-8 w-8"
           variant="ghost"
-          size="icon"
           onClick={toggleSidebar}
         >
-          <Menu />
+          <Menu/>
         </Button>
-        <Separator orientation="vertical" className="mr-2 h-4" />
         <Image src="/up-banner.png" alt="UP Banner" width={120} height={32} />
         Cereals Inventory
       </div>
