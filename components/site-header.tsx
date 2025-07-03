9e70725fa4af459c477a4ea9cd8f929a05dd2ad2
zachglindro <zmglindro2@up.edu.ph>
2025-07-03 18:35:39 +0800

    refactor(site-header): improve layout and import structure in SiteHeader component

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSidebar } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
+import { Menu, MoreVertical } from "lucide-react";
 
 import Image from "next/image";
 
@@ -10,12 +10,17 @@ export function SiteHeader() {
 
   return (
     <header className="bg-background sticky top-0 z-50 flex w-full items-center border-b">
-      <div className="flex h-(--header-height) w-full items-center gap-2 px-4">
-        <Button variant="ghost" onClick={toggleSidebar}>
-          <Menu />
+      <div className="flex h-(--header-height) w-full items-center justify-between gap-2 px-4">
+        <div className="flex items-center gap-2">
+          <Button variant="ghost" onClick={toggleSidebar}>
+            <Menu />
+          </Button>
+          <Image src="/up-banner.png" alt="UP Banner" width={120} height={32} />
+          Cereals Inventory
+        </div>
+        <Button variant="ghost">
+          <MoreVertical />
         </Button>
-        <Image src="/up-banner.png" alt="UP Banner" width={120} height={32} />
-        Cereals Inventory
       </div>
     </header>
   );
